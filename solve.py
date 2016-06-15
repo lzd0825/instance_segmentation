2623dcc40957cab6fef4382f3df20c6bc5baaa54
yangyi02 <yangyi02@gmail.com>
2016-06-15 14:40:25 -0700

    fcn8s in my folder

---
+++ solve.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('/home/yi/code/tools/caffe/python')
+import caffe
+import surgery, score
+
+import numpy as np
+import os
+
+import setproctitle
+setproctitle.setproctitle(os.path.basename(os.getcwd()))
+
+weights = '../voc-fcn16s/voc-fcn16s.caffemodel'
+
+# init
+caffe.set_device(int(sys.argv[1]))
+caffe.set_mode_gpu()
+
+solver = caffe.SGDSolver('solver.prototxt')
+solver.net.copy_from(weights)
+
+# surgeries
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+surgery.interp(solver.net, interp_layers)
+
+# scoring
+val = np.loadtxt('../data/segvalid11.txt', dtype=str)
+
+for _ in range(25):
+    solver.step(4000)
+    score.seg_tests(solver, False, val, layer='score')
