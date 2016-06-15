8c23d8f9ab3133a9a24fbb752d297d4eafc17f1c
yangyi02 <yangyi02@gmail.com>
2016-06-15 15:24:34 -0700

    add voc-fcn32s

---
+++ voc-fcn32s/solve.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('/home/yi/code/tools/caffe/python')
+import caffe
+import surgery, score
+
+import numpy as np
+import os
+
+import setproctitle
+setproctitle.setproctitle(os.path.basename(os.getcwd()))
+
+weights = '../vgg16fc.caffemodel'
+
+# init
+# caffe.set_device(int(sys.argv[1]))
+caffe.set_mode_gpu()
+caffe.set_device(1)
+
+solver = caffe.SGDSolver('solver.prototxt')
+solver.net.copy_from(weights)
+
+# surgeries
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+surgery.interp(solver.net, interp_layers)
+
+# scoring
+# val = np.loadtxt('../data/segvalid11.txt', dtype=str)
+val = np.loadtxt('/media/yi/DATA/data-orig/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt', dtype=str)
+
+for _ in range(25):
+    solver.step(4000)
+    score.seg_tests(solver, False, val, layer='score')
